d6a63777bf932f639a0c7bb16653c5f24ec6afb3
Alisher <alisher110301@gmail.com>
2022-03-16 18:53:54 +0600

    initial commit

--- diplomaBackend/settings.py
+++ diplomaBackend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +28,7 @@ SECRET_KEY = 'django-insecure-3+14wr2n93w5w*^&x*m2-a8$cx$f(y6&(^_fmyd72q6642)#2d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,3 +124,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
