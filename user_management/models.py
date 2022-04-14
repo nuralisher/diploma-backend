b3932503a822b9a30285a3eccec623d84415d22b
Alisher <alisher110301@gmail.com>
2022-04-14 16:31:30 +0600

    added: Employee toString

--- user_management/models.py
+++ user_management/models.py
@@ -11,3 +11,6 @@ class Employee(models.Model):
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.first_name
