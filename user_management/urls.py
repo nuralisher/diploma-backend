01066a2b7af557bbdb4d0d53b6e8d07e730f46f2
Alisher <alisher110301@gmail.com>
2022-04-24 22:53:46 +0600

    fix: endpoint slash

--- user_management/urls.py
+++ user_management/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from user_management.views import ClientRegistrationView, EmployeeRegistrationView
 
 urlpatterns = [
-    path('client/registration', ClientRegistrationView.as_view()),
-    path('employee/registration', EmployeeRegistrationView.as_view()),
+    path('client/registration/', ClientRegistrationView.as_view()),
+    path('employee/registration/', EmployeeRegistrationView.as_view()),
 ]
